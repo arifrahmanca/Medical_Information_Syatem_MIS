cf845c0c399037d255a4e66000c830d195e5462b
Arif Rahman <54083157+arifrahmanca@users.noreply.github.com>
2021-01-21 23:00:17 -0500

    login fixed

--- src/ctrl/Mis.java
+++ src/ctrl/Mis.java
@@ -23,16 +23,18 @@ import model.MIS;
 /**
  * Servlet implementation class Mis
  */
-@WebServlet({"/Mis", "/Mis/"})
+@WebServlet({"/Mis", "/Mis/", "/Mis/*"})
 public class Mis extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private MIS model;
 	
     boolean isLogged = false;
+    boolean isWrongPassword = false;
     boolean isLoginFailed = true;
     boolean isFailedSignup = false;
     boolean isPasswordMismatched = false;
     boolean isUpdateError = false;
+    
     
     String updateErrorMsg = "";
     
@@ -62,10 +64,13 @@ public class Mis extends HttpServlet {
 		String loginButton = request.getParameter("login-button");
 		String logoutButton = request.getParameter("logoutNav");
 		String username = request.getParameter("login-user-name"); 
+		String password = request.getParameter("login-password");
 		String createAccountButton = request.getParameter("createAccountButton");
 		String profileButton = request.getParameter("profileButton");
 		String updateButton = request.getParameter("updateButton");
 		String saveProfileButton = request.getParameter("saveProfileButton");
+		String uri = request.getRequestURI();
+		String adminLoginButton = request.getParameter("admin-login-button");
 		
 		HttpSession session = request.getSession();
 		
@@ -79,7 +84,7 @@ public class Mis extends HttpServlet {
 		if (loginNav != null) {
 			request.getRequestDispatcher("/Login.jspx").forward(request, response);
 		} else if (loginButton != null) {
-			validateLogin(username);
+			validateLogin(username, password);
 			request.setAttribute("isLogged", isLogged);
 			request.setAttribute("isLoginFailed", isLoginFailed);
 			PatientBean patient = null;
@@ -124,8 +129,26 @@ public class Mis extends HttpServlet {
 			} else {
 				request.getRequestDispatcher("/ProfilePage.jspx").forward(request, response);
 			}
-		}
-		else {
+		} else if (adminLoginButton != null) {
+			validateLogin(username, password);
+			request.setAttribute("isLogged", isLogged);
+			request.setAttribute("isLoginFailed", isLoginFailed);
+			PatientBean patient = null; 
+			if (isLogged) { 
+				try {
+					patient = model.retrieveUserInfo(username);
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				session.setAttribute("patient", patient);
+				request.getRequestDispatcher("/ProfilePage.jspx").forward(request, response);
+			} else {
+				request.getRequestDispatcher("/AdminLogin.jspx").forward(request, response);
+			}
+		} else if (uri.contains("Admin")) {
+			isLoginFailed = false;
+			request.getRequestDispatcher("/AdminLogin.jspx").forward(request, response);
+		} else {
 			request.getRequestDispatcher("/index.jspx").forward(request, response);
 		}
 	}
@@ -157,18 +180,33 @@ public class Mis extends HttpServlet {
 		return users;
 	}
 	
-	private void validateLogin(String username) {
-		if (isUserExist(username)) {
+	private void validateLogin(String username, String password) {
+		if (isValidLogin(username, password)) {
 			isLoginFailed = false;
 			isLogged = true;
+		} else {
+			isLoginFailed = true;
+			isLogged = false;
 		}
+	}
+	
+	private boolean isValidLogin(String username, String password) {
+		List<UserBean> users = getUsers();
+		for (UserBean user : users) {
+			if (user.getUsername().equals(username)) {
+				if (user.getPassword().equals(password)) {
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 	
 	private boolean isUserExist(String username) {
 		List<UserBean> users = getUsers();
 		for (UserBean user : users) {
 			if (user.getUsername().equals(username)) {
-				return true;
+					return true;
 			}
 		}
 		return false;
@@ -199,7 +237,7 @@ public class Mis extends HttpServlet {
 		} else {
 			isFailedSignup = false;
 			isPasswordMismatched = false;
-			model.getPatientData().insertPatient(firstName, lastName, phone, email);
+			model.getPatientData().insertPatient(lastName, firstName, phone, email);
 			model.getUserData().insertUser(registerUsername, registerPassword);
 			model.getPatientData().insertAddress(street, city, province, zip, country);
 		}
